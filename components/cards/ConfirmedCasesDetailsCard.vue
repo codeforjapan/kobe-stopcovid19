b41a92fa00d1f1b5b839d382ef423d973e43f6fd
y-chan <y_chan_crypto@yahoo.co.jp>
2020-04-10 17:11:43 +0900

    add open data link and correspond new format of data(update script)

--- components/cards/ConfirmedCasesDetailsCard.vue
+++ components/cards/ConfirmedCasesDetailsCard.vue
@@ -4,17 +4,11 @@
       :title="$t('検査陽性者の状況')"
       :title-id="'details-of-confirmed-cases'"
       :date="Data.patients.date"
-      :url="
-        'https://www.city.kobe.lg.jp/a73576/kenko/health/infection/protection/covid_19.html'
-      "
     >
       <template v-slot:button>
         <ul :class="$style.note">
           <li>
             {{ $t('注）神戸市外在住者は含まれていない') }}
-          </li>
-          <li>
-            {{ $t('注）「検査件数総数」には、検査中のものを含みます。') }}
           </li>
           <li>
             {{
@@ -29,6 +23,13 @@
         :aria-label="$t('検査陽性者の状況')"
         v-bind="confirmedCases"
       />
+      <template v-slot:footer>
+        <open-data-link
+          :url="
+            'https://www.city.kobe.lg.jp/a73576/kenko/health/infection/protection/covid_19.html#shinaijokyo'
+          "
+        />
+      </template>
     </data-view>
   </v-col>
 </template>
@@ -52,11 +53,13 @@ import Data from '@/data/data.json'
 import formatConfirmedCases from '@/utils/formatConfirmedCases'
 import DataView from '@/components/DataView.vue'
 import ConfirmedCasesDetailsTable from '@/components/ConfirmedCasesDetailsTable.vue'
+import OpenDataLink from '@/components/OpenDataLink'
 
 export default {
   components: {
     DataView,
-    ConfirmedCasesDetailsTable
+    ConfirmedCasesDetailsTable,
+    OpenDataLink
   },
   data() {
     // 検査陽性者の状況
