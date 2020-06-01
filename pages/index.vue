a610bd52ae29531715a34789f343f8ef64557dcb
MaySoMusician <maysomusician@gmail.com>
2020-06-01 17:04:40 +0900

    Remove unnecessary components & data

--- pages/index.vue
+++ pages/index.vue
@@ -4,18 +4,7 @@
       <page-header :icon="headerItem.icon">
         {{ headerItem.title }}
       </page-header>
-      <div class="UpdatedAt">
-        <span>{{ $t('最終更新') }} </span>
-        <time :datetime="updatedAt">{{ Data.lastUpdate }}</time>
-      </div>
-      <div
-        v-show="!['ja', 'ja-basic'].includes($i18n.locale)"
-        class="Annotation"
-      >
-        <span>{{ $t('注釈') }} </span>
-      </div>
     </div>
-    <whats-new class="mb-4" :items="newsItems" />
     <div class="mb-4" style="margin-top:50px">
       <p style="font-size:1.875rem;">
         「新型コロナウイルス感染症対策サイト」は2020年6月1日より神戸市公式ホームページに統合しました。<br />
@@ -27,19 +16,6 @@
         >
       </p>
     </div>
-    <v-row class="DataBlock">
-      <confirmed-cases-details-card />
-      <!--<tested-cases-details-card />-->
-      <confirmed-cases-attributes-card />
-      <confirmed-cases-number-card />
-      <!--<inspection-persons-number-card />-->
-      <tested-number-card />
-      <telephone-advisory-reports-number-card />
-      <!--<consultation-desk-reports-number-card />
-      <metro-card />
-      <agency-card />-->
-      <health-center-desk-reports-number-card />
-    </v-row>
   </div>
 </template>
 
@@ -47,16 +23,6 @@
 import Vue from 'vue'
 import { MetaInfo } from 'vue-meta'
 import PageHeader from '@/components/PageHeader.vue'
-import Data from '@/data/data.json'
-import News from '@/data/news.json'
-// import TestedCasesDetailsCard from '@/components/cards/TestedCasesDetailsCard.vue'
-// import InspectionPersonsNumberCard from '@/components/cards/InspectionPersonsNumberCard.vue'
-/*
-import ConsultationDeskReportsNumberCard from '@/components/cards/ConsultationDeskReportsNumberCard.vue'
-import MetroCard from '@/components/cards/MetroCard.vue'
-import AgencyCard from '@/components/cards/AgencyCard.vue'
- */
-import { convertDatetimeToISO8601Format } from '@/utils/formatDate'
 
 export default Vue.extend({
   components: {
@@ -64,19 +30,12 @@ export default Vue.extend({
   },
   data() {
     const data = {
-      Data,
       headerItem: {
         icon: 'mdi-chart-timeline-variant',
         title: this.$t('神戸市市内の最新感染動向')
-      },
-      newsItems: News.newsItems
+      }
     }
     return data
-  },
-  computed: {
-    updatedAt() {
-      return convertDatetimeToISO8601Format(this.$data.Data.lastUpdate)
-    }
   },
   head(): MetaInfo {
     return {
@@ -96,35 +55,6 @@ export default Vue.extend({
     @include lessThan($small) {
       flex-direction: column;
       align-items: baseline;
-    }
-  }
-
-  .UpdatedAt {
-    @include font-size(14);
-
-    color: $gray-3;
-    margin-bottom: 0.2rem;
-  }
-
-  .Annotation {
-    @include font-size(12);
-
-    color: $gray-3;
-    @include largerThan($small) {
-      margin: 0 0 0 auto;
-    }
-  }
-  .DataBlock {
-    margin: 20px -8px;
-
-    .DataCard {
-      @include largerThan($medium) {
-        padding: 10px;
-      }
-
-      @include lessThan($small) {
-        padding: 4px 8px;
-      }
     }
   }
 }
